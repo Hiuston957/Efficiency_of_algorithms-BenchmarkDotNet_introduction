fa50940427ab7af42e816a8e93c98daacafcaa25
Luke <blackeyesblackbear@gmail.com>
2024-03-29 00:32:54 +0100

    Add project files.

---
+++ Efficiency_of_algorithms/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using BenchmarkDotNet.Running;
+
+namespace Efficiency_of_algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] myIntArray = { 5, 2, 8, 1, 6 };
+            Generators XD = new Generators();
+
+           // XD.showArray();
+            //XD.insertionSort();
+
+
+           XD.quickSort();
+         
+       
+          //  XD.showArray();
+
+            var results = BenchmarkRunner.Run<Generators>();
+
+
+
+
+        }
+    }
+}
